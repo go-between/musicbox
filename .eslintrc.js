9fa833ac39a771a486cd0b689f6b5185d1380a2c
Truman Shuck <hello@trumanshuck.com>
2020-01-03 11:40:09 -0600

    Ease up on function return eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,14 +13,22 @@ module.exports = {
       jsx: true, // Allows for the parsing of JSX
     },
   },
+  plugins: ['react-hooks'],
   env: {
     es6: true,
     browser: true,
     node: true,
   },
   rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "@typescript-eslint/explicit-function-return-type": [
+      "warn",
+      {
+        "allowExpressions": true,
+        "allowTypedFunctionExpressions": true,
+      }
+    ]
   },
   settings: {
     react: {
