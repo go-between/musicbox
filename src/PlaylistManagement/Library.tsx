4ffa9390968da70bb78e7c2b6203c968e88b63d4
Truman Shuck <hello@trumanshuck.com>
2020-01-22 18:13:21 -0600

    Allow songs to be removed from a user's library

--- src/PlaylistManagement/Library.tsx
+++ src/PlaylistManagement/Library.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
-import { useQuery } from '@apollo/react-hooks'
+import { useQuery, useMutation } from '@apollo/react-hooks'
 
 import { RoomPlaylistRecordsReorder } from './PlaylistManagement'
-import { SONGS_QUERY, OrderedRecord, RoomPlaylistRecord, SongsQuery } from './graphql'
+import {
+  SONGS_QUERY,
+  USER_LIBRARY_RECORD_DELETE_MUTATION,
+  OrderedRecord,
+  RoomPlaylistRecord,
+  SongsQuery,
+  UserLibraryRecordDeleteMutation,
+} from './graphql'
 
 type Props = {
   roomPlaylistRecordsReorder: RoomPlaylistRecordsReorder[0]
@@ -10,6 +17,10 @@ type Props = {
 }
 const Library: React.FC<Props> = ({ roomPlaylistRecordsReorder, roomPlaylistRecords }) => {
   const { data, loading } = useQuery<SongsQuery['data']>(SONGS_QUERY)
+  const [userLibraryRecordDelete] = useMutation<
+    UserLibraryRecordDeleteMutation['data'],
+    UserLibraryRecordDeleteMutation['vars']
+  >(USER_LIBRARY_RECORD_DELETE_MUTATION, { refetchQueries: ['SongsQuery'] })
 
   if (loading) {
     return <p>Loading...</p>
@@ -26,9 +37,14 @@ const Library: React.FC<Props> = ({ roomPlaylistRecordsReorder, roomPlaylistReco
 
       roomPlaylistRecordsReorder({ variables: { orderedRecords } })
     }
+
+    const deleteSong = (): void => {
+      userLibraryRecordDelete({ variables: { id: s.id } })
+    }
     return (
       <li key={s.id}>
-        {s.name} <button onClick={addSong}>Add</button>{' '}
+        {s.name} <button onClick={addSong}>Enqueue</button>
+        <button onClick={deleteSong}>Delete from Library</button>{' '}
       </li>
     )
   })
