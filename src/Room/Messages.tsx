1c6f40f59f32229d8c54aa24a60b6a4cb1a0791d
Truman Shuck <hello@trumanshuck.com>
2020-01-25 00:22:42 -0600

    Subscribe messages to the message websocket

--- src/Room/Messages.tsx
+++ src/Room/Messages.tsx
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import moment from 'moment'
+
+import { WebsocketContext } from 'App'
 
 import { MESSAGES_QUERY, MessagesQuery, Message } from './graphql'
 
 const Messages: React.FC = () => {
   const { data, loading } = useQuery<MessagesQuery['data'], MessagesQuery['vars']>(MESSAGES_QUERY)
   const [messages, setMessages] = useState<Message[]>([])
+  const [newMessage, setNewMessage] = useState<Message | null>(null)
 
   useEffect(() => {
     if (!data) {
@@ -16,13 +19,34 @@ const Messages: React.FC = () => {
     setMessages(data.messages)
   }, [data])
 
+  useEffect(() => {
+    if (!newMessage) {
+      return
+    }
+
+    const newMessages = [...messages, newMessage].sort((prev, next) => (prev.createdAt > next.createdAt ? 0 : 1))
+    setMessages(newMessages)
+    setNewMessage(null)
+  }, [messages, newMessage])
+
+  const websocket = useContext(WebsocketContext)
+  useEffect(() => {
+    if (!websocket) {
+      return
+    }
+
+    return websocket.subscribeToMessage(message => {
+      setNewMessage(message)
+    })
+  }, [websocket])
+
   if (loading) {
     return <p>Loading...</p>
   }
 
   const messageLines = messages.map(message => {
     const withSong = message.roomPlaylistRecord && <i>during {message.roomPlaylistRecord.song.name}</i>
-    const displayDate = moment(message.createdAt).format('ddd h:m a')
+    const displayDate = moment(message.createdAt).format('ddd h:mm a')
     return (
       <div key={message.id}>
         <p>
